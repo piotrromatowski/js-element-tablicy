95996863ac20657aec7e9a7c94a1be7bd5309ab2
Piotr <piotr.romatowski@gmail.com>
2020-02-11 18:51:24 +0100

    js-element-tablicy

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let tablica = [57,62,73,84,95];
 
-let parametr = function (tablica) {
-    let result = [0];
+let element = function (tablica) {
+    let result =(tablica[0]);
     return result;
 };
-console.log(tablica[0]);
+console.log(element(tablica));
